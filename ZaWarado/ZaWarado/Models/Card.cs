6051a331daf1170a0785c1fc749e270786244940
EternityDoom <EternityDoom@users.noreply.github.com>
2020-05-20 19:02:30 -0600

    Added the Card class.

---
+++ ZaWarado/ZaWarado/Models/Card.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ZaWarado.Models
+{
+    /// <summary>
+    /// Represents a single card in the game. Its main property is its type, since its image is
+    /// determined based on which type it is.
+    /// </summary>
+    public class Card
+    {
+        private static ImageSource[] cardImages = new ImageSource[Enum.GetValues(typeof(Type)).Length];
+
+        /// <summary>
+        /// Represents the type of a Card. A Card's individual effect, and the effects that occur
+        /// based on adjacent cards, are determined by its type.
+        /// </summary>
+        public enum Type
+        {
+            PLANT,
+            WATER,
+            WIND,
+            FIRE,
+            MOUNTAIN,
+            PLAINS
+        }
+
+        /// <summary>
+        /// Which type of Card this is, such as PLANT. Use this for determining effects that
+        /// occur when this Card is played. A Card's type never changes after it's created,
+        /// so this property is read only.
+        /// </summary>
+        public Type type { get; private set; }
+
+        /// <summary>
+        /// The source image file that will be used to display this Card. The actual display
+        /// is handled by an Image object. The file used is determined based on the Card's
+        /// type, so this property is read only.
+        /// </summary>
+        public ImageSource imageFile
+        {
+            get
+            {
+                // Add code later for loading in an image if it hasn't been loaded yet.
+
+                return cardImages[(int)type];
+            }
+        }
+
+        /// <summary>
+        /// Creates a new Card with a given type. Remember, the type is permanent once set.
+        /// </summary>
+        /// <param name="type">The type of Card to create.</param>
+        public Card(Type type)
+        {
+            this.type = type;
+        }
+
+        /// <summary>
+        /// Treats a Card as if it were its type.
+        /// </summary>
+        /// <param name="card">The Card to use as its type.</param>
+        public static implicit operator Type(Card card) 
+        {
+            return card.type;
+        }
+    }
+}
