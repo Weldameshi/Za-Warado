980ce81c383b14b3c0bf585f49a26d4ef1ddf1d1
Joshua Favre <jfavre390@gmail.com>
2020-05-21 13:05:04 -0400

    More board functonality

--- ZaWarado/Board.cs
+++ ZaWarado/Board.cs
@@ -7,7 +7,16 @@ internal class Card { }
 
 public class Board
 {
-    Card[,] board = null;
+    internal struct Coord
+    {
+        internal int x;
+        internal int y;
+
+        internal Coord(int x, int y) { this.x = x; this.y = y; }
+    }
+
+    //Card[,] board = null;
+    Dictionary<Coord, Card> board = null;
 
     /// <summary>
     /// Get the instance of card at the given location on the board
@@ -41,4 +50,43 @@ public class Board
     /// <param name="horizontalBoardDimension">Horizontal board dimension</param>
     /// <param name="verticalBoardDimension">Vertical board dimension</param>
     public Board(int horizontalBoardDimension, int verticalBoardDimension) => board = new bool[horizontalBoardDimension, verticalBoardDimension];
+
+    /// <summary>
+    /// Add a card to the board in the requested location
+    /// </summary>
+    /// <param name="card">Card to add to the board</param>
+    /// <param name="x">X location to add the card at</param>
+    /// <param name="y">Y location to add the card at</param>
+    public void AddCard(Card card, int x, int y)
+    {
+        if (board is null)
+            return;
+        if (x < 0 || x >= board.GetLength(0))
+            throw new ArgumentOutOfRangeException(nameof(x), x, "provided value cannot be less than zero, or greater than the last index of the array");
+        if (y < 0 || y >= board.GetLength(1))
+            throw new ArgumentOutOfRangeException(nameof(y), y, "provided value cannot be less than zero, or greater than the last index of the array");
+
+        board[x, y] = card;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
+    public Card[,] GetCardAndNeighbors(int x, int y)
+    {
+        if (board is null)
+            return;
+        if (x < 0 || x >= board.GetLength(0))
+            throw new ArgumentOutOfRangeException(nameof(x), x, "provided value cannot be less than zero, or greater than the last index of the array");
+        if (y < 0 || y >= board.GetLength(1))
+            throw new ArgumentOutOfRangeException(nameof(y), y, "provided value cannot be less than zero, or greater than the last index of the array");
+
+        Card[,] neighbors = new Card[3, 3];
+        
+
+
+    }
 }
---
+++ ZaWarado/ZaWarado/Models/Board.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZaWarado.Models
+{
+    public class Board
+    {
+        /// <summary>
+        /// Internal data structure used to track the location of valid cards
+        /// </summary>
+        internal struct Coord
+        {
+            internal int x;
+            internal int y;
+
+            internal Coord(int x, int y) { this.x = x; this.y = y; }
+        }
+
+        //Card[,] board = null;
+        Dictionary<Coord, Card> board = null;
+
+        /// <summary>
+        /// Get the instance of card at the given location on the board
+        /// </summary>
+        /// <param name="x">X location of the desired card</param>
+        /// <param name="y">Y location of the desired card</param>
+        /// <returns>The card instance found at the provided location</returns>
+        public Card this[int x, int y]
+        {
+            get
+            {
+                if (board is null)
+                    return null;
+
+                Coord loc = new Coord(x, y);
+                if (board.ContainsKey(loc))
+                    return board[loc];
+                else return null;
+            }
+        }
+
+        /// <summary>
+        /// Create a square board of the given dimension
+        /// </summary>
+        /// <param name="squareBoardDimensions">Square boarddimension</param>
+        public Board(int squareBoardDimensions) => board = new Dictionary<Coord, Card>();
+        /// <summary>
+        /// Create a rectangular board of the given dimensions
+        /// </summary>
+        /// <param name="horizontalBoardDimension">Horizontal board dimension</param>
+        /// <param name="verticalBoardDimension">Vertical board dimension</param>
+        public Board(int horizontalBoardDimension, int verticalBoardDimension) => board = new Dictionary<Coord, Card>();
+
+        /// <summary>
+        /// Add a card to the board in the requested location
+        /// </summary>
+        /// <param name="card">Card to add to the board</param>
+        /// <param name="x">X location to add the card at</param>
+        /// <param name="y">Y location to add the card at</param>
+        public void AddCard(Card card, int x, int y)
+        {
+            if (board is null)
+                return;
+
+            Coord loc = new Coord(x, y);
+            if (board.ContainsKey(loc))
+                return;
+
+            board[loc] = card;
+        }
+
+        /// <summary>
+        /// Return a card and it's paralell neighbors
+        /// </summary>
+        /// <param name="x">Central card X location</param>
+        /// <param name="y">Central card Y location</param>
+        /// <returns>A collection of non-null neighbors where applicable</returns>
+        public Card[,] GetCardAndNeighbors(int x, int y)
+        {
+            if (board is null)
+                return null;
+
+            Card[,] neighbors = new Card[3, 3];
+            Coord loc = new Coord(x, y);
+            if (board.ContainsKey(loc))
+            {
+                neighbors[1, 1] = board[loc];                             Coord next = new Coord(x, y - 1);
+                neighbors[1, 0] = board.ContainsKey(next) ? board[next] : null; next = new Coord(x, y + 1);
+                neighbors[1, 2] = board.ContainsKey(next) ? board[next] : null; next = new Coord(x - 1, y);
+                neighbors[0, 1] = board.ContainsKey(next) ? board[next] : null; next = new Coord(x + 1, y);
+                neighbors[2, 1] = board.ContainsKey(next) ? board[next] : null;
+                return neighbors;
+            }
+            return null;
+        }
+    }
+}
