b839a8a3c9e2a4da18c4a1c2be4eaad4e91c93e6
Joshua Favre <jfavre390@gmail.com>
2020-05-25 15:08:54 -0400

    Add Microsoft Test project

---
+++ ZaWarado/UnitTesting/BoardTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class BoardTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
